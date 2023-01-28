4c222aff19743c2016685eeb49466debbbff8b3e
James Mitsumasu <jrmitsumasu@gmail.com>
2023-01-27 22:23:01 -0500

    Created Guessing game via Java

---
+++ src/com/coderscampus/assignment2/GuessingGame.java
@@ -0,0 +1,55 @@
+package com.coderscampus.assignment2;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingGame {
+
+	public static void main(String[] args) {
+		
+		//Need input, need target number, need while loop, 5 guesses, conditional for higher/lower/equal
+		//Only use ints
+		
+		int numberOfGuesses = 1;
+		
+		Random target = new Random();
+		
+		int theRandomNumber = target.nextInt(100) + 1;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Pick a number between 1 and 100 ");
+		
+		String userInput = input.nextLine();
+		
+		int userValue = Integer.parseInt(userInput);
+
+		while((userValue != theRandomNumber) && (numberOfGuesses < 5)) {
+			
+			if (userValue > 100 || userValue < 1) {
+				System.out.println("Your guess is not between 1 and 100, please try again");
+			}
+			else {
+				if (userValue < theRandomNumber) {
+					System.out.println("Please pick a higher number");
+				}
+				else if(userValue > theRandomNumber) {
+					System.out.println("Please pick a lower number");
+				}
+			}
+			userValue = Integer.parseInt(input.nextLine());
+			numberOfGuesses++;
+		}
+		
+		//checks for match of number last try and only if wrong
+		if (numberOfGuesses == 5 && userValue != theRandomNumber) {
+			System.out.println("You lose, the number to guess was " + theRandomNumber);
+		}
+		
+		if (userValue == theRandomNumber) {
+			System.out.println("You win!");
+		}
+		
+		input.close();
+	}
+}
